1dd25e4abd1ee32488734ef0a4ce322ca79471f8
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2020-01-29 20:25:38 +0100

    .

---
+++ testing-04-junit/src/test/java/org/course/testing/AccountTest.java
@@ -0,0 +1,35 @@
+package org.course.testing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AccountTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testEmptyAccountDeposit() {
+    	Account account = new Account();
+    	account.ingresar(100.0d);
+        assertTrue(account.getBalance() == 100.0d);
+    }
+}
